1fb2d41b07077a5eda3ac1953be08dd44a345f20
Eddiecxl <127755170+Eddiecxl@users.noreply.github.com>
2026-09-22 15:20:45 +0800

    release: bump tested WebView hotfix to v30.0.5

--- hotfix-overlay/internal/config/version.go
+++ hotfix-overlay/internal/config/version.go
@@ -14,8 +14,8 @@ type BuildInfo struct {
 	PatchFormat     int
 }
 
-var buildGameVersion = "30.0.4"
-var buildLauncherVersion = "5.1.3"
+var buildGameVersion = "30.0.5"
+var buildLauncherVersion = "5.1.4"
 
 var Current = BuildInfo{
 	GameVersion:     buildGameVersion,
